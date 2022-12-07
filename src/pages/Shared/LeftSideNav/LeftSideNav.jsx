a25efcfd0d3d0e12627d0869699656ba74fa2c53
NahidulNoman <nahidulna5@gmail.com>
2022-12-07 23:14:29 +0600

    left side nav edited

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-    const [category , setCategory] = useState([]);
+  const [category, setCategory] = useState([]);
 
-    useEffect( () => {
-        fetch('https://fire-news-server.vercel.app/category')
-        .then(res => res.json())
-        .then(data => setCategory(data))
-    },[])
+  useEffect(() => {
+    fetch("https://fire-news-server.vercel.app/category")
+      .then((res) => res.json())
+      .then((data) => setCategory(data));
+  }, []);
 
-    return (
-        <div>
-            <p>All Category : {category.length}</p>
-            {
-                category.map(item => <p key={item.id}>
-                    <Link to={`category/${item.id}`}>{item.name}</Link>
-                </p>)
-            }
-        </div>
-    );
+  return (
+    <div>
+      <p>All Category : {category.length}</p>
+      {category.map((item) => (
+        <p key={item.id}>
+          <Link to={`category/${item.id}`}>{item.name}</Link>
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default LeftSideNav;
