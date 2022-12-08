ea8cd97aaeaed2666dd1fbd54004d0a27f64e87b
NahidulNoman <nahidulna5@gmail.com>
2022-12-08 19:47:25 +0600

    show new cart on home page

--- src/pages/Shared/ShowNewsCart/ShowNewsCart.jsx
+++ src/pages/Shared/ShowNewsCart/ShowNewsCart.jsx
@@ -5,7 +5,7 @@ import { FaEye, FaRegBookmark, FaShareAlt, FaStar } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const ShowNewsCart = ({ news }) => {
-  const { title, total_view, author,rating,_id, details, image_url } = news;
+  const { title, total_view, author, rating, _id, details, image_url } = news;
   return (
     <div className="mb-3">
       <Card className="">
@@ -29,26 +29,28 @@ const ShowNewsCart = ({ news }) => {
         </Card.Header>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
-            <Card.Img variant="top" src={image_url} />
+          <Card.Img variant="top" src={image_url} />
           <Card.Text>
-            {
-                        details.length > 250 ?
-                            <>{details.slice(0, 250) + '...'} <Link to={`/news/${_id}`}>Read More</Link> </>
-                            :
-                            details
-                    }
+            {details.length > 250 ? (
+              <>
+                {details.slice(0, 250) + "..."}{" "}
+                <Link to={`/news/${_id}`}>Read More</Link>{" "}
+              </>
+            ) : (
+              details
+            )}
           </Card.Text>
         </Card.Body>
         <Card.Footer className="d-flex justify-content-between">
-                <div>
-                    <FaStar className='text-warning me-2'></FaStar>
-                    <span>{rating?.number}</span>
-                </div>
-                <div>
-                    <FaEye className='me-2'></FaEye>
-                    <span>{total_view}</span>
-                </div>
-            </Card.Footer>
+          <div>
+            <FaStar className="text-warning me-2"></FaStar>
+            <span>{rating?.number}</span>
+          </div>
+          <div>
+            <FaEye className="me-2"></FaEye>
+            <span>{total_view}</span>
+          </div>
+        </Card.Footer>
       </Card>
     </div>
   );
