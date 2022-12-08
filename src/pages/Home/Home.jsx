3f4ccdc999d53e31adfea7bd117c2f662d5dff11
NahidulNoman <nahidulna5@gmail.com>
2022-12-08 19:44:08 +0600

    home news

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import ShowNewsCart from '../Shared/ShowNewsCart/ShowNewsCart';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import ShowNewsCart from "../Shared/ShowNewsCart/ShowNewsCart";
 
 const Home = () => {
-    const allNews = useLoaderData();
-    return (
-        <div>
-            <h2>all fire News Home: {allNews.length}</h2>
-            {
-                allNews.map(news => <ShowNewsCart
-                    key={news._id}
-                    news={news}
-                ></ShowNewsCart>)
-            }
-        </div>
-    );
+  const allNews = useLoaderData();
+  return (
+    <div>
+      <h2>all fire News Home: {allNews.length}</h2>
+      {allNews.map((news) => (
+        <ShowNewsCart key={news._id} news={news}></ShowNewsCart>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
